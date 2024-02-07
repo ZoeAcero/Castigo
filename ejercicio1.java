c3622709e925e89952158f5808fb17c13f041691
ZoeAcero <zar.amazona@gmail.com>
2024-02-07 12:19:56 +0100

    estoy muy enfadado por quien halla puesto el video

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,3 +1,18 @@
-public class ejercicio1 {
+public class Main {
+    public static void main(String[] args) {
+        int diasEnUnAnio = 365;
+        int horasEnUnDia = 24;
+        int minutosEnUnaHora = 60;
+        int segundosEnUnMinuto = 60;
+
+        int segundosEnUnAnio = calcularSegundosEnUnAnio(diasEnUnAnio, horasEnUnDia, minutosEnUnaHora, segundosEnUnMinuto);
+
+        System.out.println("El número de segundos en un año es: " + segundosEnUnAnio);
+    }
+
+    public static int calcularSegundosEnUnAnio(int dias, int horas, int minutos, int segundos) {
+        int segundosEnUnDia = horas * minutos * segundos;
+        return dias * segundosEnUnDia;
+    }
 }
 
